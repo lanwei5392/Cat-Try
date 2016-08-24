c15b455f9f470a4f718da8ecd1a1a2fb2bd11099
lanwei5392 <lanweius@yahoo.com>
2016-08-23 23:28:01 -0700

    upload cat try

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Enable this js file to strict mode
+function render() {
+	var	catImages = [
+		'images/cat-1.jpg',
+		'images/cat-2.jpg',
+		'images/cat-3.jpg'，
+		'images/cat-4.jpg',
+		'images/cat-5.jpg'
+	]
+}
